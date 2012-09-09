c13c8d51f5b5dc0adcdae22765ddfbf93d24059a
Matias Alberto Mendoza <matuteworld@gmail.com>
2012-09-09 20:53:35 -0300

    Vista de OC - actualizada
    Falta corregir, errores en la maquetacion.

--- application/views/oc_view.php
+++ application/views/oc_view.php
@@ -1,1 +1,90 @@
-orden de compra - VIEW
+<h3>Generador de Ordenes de compra</h3>
+
+<h4>NOTAS DE PEDIDO APROBADAS</h4>
+
+<div class="pagination">
+  <ul>
+    <li><a href="#">Prev</a></li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">Next</a></li>
+  </ul>
+</div>
+
+<table class="table table-bordered">
+  <tr class="info">
+    <td></td>
+    <td>Nro. N.P.</td>
+    <td>Fec.Carga</td>
+    <td>Estado</td>
+    <td>Exp. N°</td>
+    <td>Oficina</td>
+    <td>Usuario</td>
+  </tr>
+  <tr>
+    <td>Enviar Consulta</td>
+    <td>42</td>
+    <td>06/09/2011</td>
+    <td>APROBADA</td>
+    <td>1-5049-2011</td>
+    <td>MANTENIMIENTO - CASA CENTRAL</td>
+    <td>AYUP CYNTHIA INES</td>
+  </tr>
+  <tr>
+    <td>Enviar Consulta</td>
+    <td>41</td>
+    <td>05/09/2011</td>
+    <td>APROBADA</td>
+    <td>1-2536-2011</td>
+    <td>GERENCIA DEL C.C.E. - CASA CENTRAL  </td>
+    <td>SALAZAR GUEMES MARIANELLA</td>
+  </tr>
+</table>
+
+<table class="table table-bordered">
+  <caption>titulo</caption>
+  <thead class="info">
+    <tr>
+      <th></th>
+      <th>Nro. N.P.</th>
+      <th>Fec.Carga</th>
+      <th>Estado</th>
+      <th>Exp. N°</th>
+      <th>Oficina</th>
+      <th>Usuario</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Enviar Consulta</td>
+      <td>42</td>
+      <td>06/09/2011</td>
+      <td>APROBADA</td>
+      <td>1-5049-2011</td>
+      <td>MANTENIMIENTO - CASA CENTRAL</td>
+      <td>AYUP CYNTHIA INES</td>
+    </tr>
+      <td>Enviar Consulta</td>
+      <td>41</td>
+      <td>05/09/2011</td>
+      <td>APROBADA</td>
+      <td>1-2536-2011</td>
+      <td>GERENCIA DEL C.C.E. - CASA CENTRAL  </td>
+      <td>SALAZAR GUEMES MARIANELLA</td>
+  <tr>
+  </tr>
+  </tbody>
+</table>
+
+<div class="pagination">
+  <ul>
+    <li><a href="#">Prev</a></li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">Next</a></li>
+  </ul>
+</div>
