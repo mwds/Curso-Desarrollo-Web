484d6932e8dbc9ec9fadc25820f2355132e9a48a
Patricio Ontiveros <ontiverospatricio@gmail.com>
2012-09-17 19:47:29 -0300

    oc_liberar

--- application/views/oc_liberar.php
+++ application/views/oc_liberar.php
@@ -1,1 +1,101 @@
 orden de compra - LIBERAR
+
+      <div class="row">
+        <div class="span12"><h4>Ordenes de Compra</h4></div>
+      </div>  
+      <div class="row">
+        <div class="span12">   
+          <div class="pagination">
+            <ul>
+              <li><a href="#">Prev</a></li>
+              <li><a href="#">1</a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#">4</a></li>
+              <li><a href="#">Next</a></li>
+            </ul>
+          </div>
+        </div>
+
+      <div class="row">
+
+        <div class="span12">
+
+          <table class="table table-bordered">
+            <tr class="info">
+              <td></td>
+              <td></td>
+              <td>Fecha Carga</td>
+              <td>Razon Social</td>
+              <td>Estado</td>
+              <td></td>
+            </tr>
+          </thead>
+            <tbody>
+              <tr>
+                <td><i class="icon-list"></i></td>
+                <td><i class="icon-pencil"></i></td>
+                <td>06/09/2011</td>
+                <td>COPITEC LIBRERIA -DE STAMP S.R.L.-</td>
+                <td>EN CONFIGURACION</td>
+                <td><button class="btn btn-primary" type="submit">Liberar</button></td>
+              </tr>
+              <tr>
+                <td><i class="icon-list"></i></td>
+                <td><i class="icon-pencil"></i></td>
+                <td>06/09/2011</td>
+                <td>LIBRERIA JUNIN -DE PABLO ANTONIO LEON-</td>
+                <td>EN CONFIGURACION</td>
+                <td><button class="btn btn-primary" type="submit">Liberar</button></td>
+              </tr>
+              <tr>
+                <td><i class="icon-list"></i></td>
+                <td><i class="icon-pencil"></i></td>
+                <td>06/09/2011</td>
+                <td>AHUALLI MARI</td>
+                <td>EN CONFIGURACION</td>
+                <td><button class="btn btn-primary" type="submit">Liberar</button></td>
+              </tr>
+              <tr>
+                <td><i class="icon-list"></i></td>
+                <td><i class="icon-pencil"></i></td>
+                <td>06/09/2011</td>
+                <td>ALUBA</td>
+                <td>EN CONFIGURACION</td>
+                <td><button class="btn btn-primary" type="submit">Liberar</button></td>
+              </tr>
+              <tr>
+                <td><i class="icon-list"></i></td>
+                <td><i class="icon-pencil"></i></td>
+                <td>06/09/2011</td>
+                <td>ABC S.A.</td>
+                <td>EN CONFIGURACION</td>
+                <td><button class="btn btn-primary" type="submit">Liberar</button></td>
+              </tr>
+              <tr>
+                <td><i class="icon-list"></i></td>
+                <td><i class="icon-pencil"></i></td>
+                <td>06/09/2011</td>
+                <td>PINTURERIA ESPAÑA S.R.L.</td>
+                <td>EN CONFIGURACION</td>
+                <td><button class="btn btn-primary" type="submit">Liberar</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+        <div class="row">
+          <div class="span12">   
+            <div class="pagination">
+              <ul>
+                <li><a href="#">Prev</a></li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">Next</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
