93c4cb784e490cf632962e558825e786f6227e5c
Patricio Ontiveros <ontiverospatricio@gmail.com>
2012-09-29 15:05:54 -0300

    oc_view solucionado problema de rows

--- application/views/oc_view.php
+++ application/views/oc_view.php
@@ -19,59 +19,63 @@
      
 
      
-          <div class="row">
-      <div class="pagination">
-        <ul>
-          <li><a href="#">Prev</a></li>
-          <li><a href="#">1</a></li>
-          <li><a href="#">2</a></li>
-          <li><a href="#">3</a></li>
-          <li><a href="#">4</a></li>
-          <li><a href="#">Next</a></li>
-        </ul>
+    <div class="row">
+      <div class="span12">
+        <div class="pagination">
+          <ul>
+            <li><a href="#">Prev</a></li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">Next</a></li>
+          </ul>
+        </div>
       </div>
     </div>
 
-                <div class="row">
-              <div class="span12">
-    <table class="table table-bordered">
-      <caption>titulo</caption>
-      <thead class="info">
-        <tr>
-          <th></th>
-          <th>Nro. N.P.</th>
-          <th>Fec.Carga</th>
-          <th>Estado</th>
-          <th>Exp. N°</th>
-          <th>Oficina</th>
-          <th>Usuario</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Enviar Consulta</td>
-          <td>42</td>
-          <td>06/09/2011</td>
-          <td>APROBADA</td>
-          <td>1-5049-2011</td>
-          <td>MANTENIMIENTO - CASA CENTRAL</td>
-          <td>AYUP CYNTHIA INES</td>
-        </tr>
-      <td>Enviar Consulta</td>
-          <td>41</td>
-          <td>05/09/2011</td>
-          <td>APROBADA</td>
-          <td>1-2536-2011</td>
-          <td>GERENCIA DEL C.C.E. - CASA CENTRAL  </td>
-          <td>SALAZAR GUEMES MARIANELLA</td>
-      <tr>
-      </tr>
-      </tbody>
+    <div class="row">
+      <div class="span12">
+        <table class="table table-bordered">
+          <caption>titulo</caption>
+            <thead class="info">
+              <tr>
+                <th></th>
+                <th>Nro. N.P.</th>
+                <th>Fec.Carga</th>
+                <th>Estado</th>
+                <th>Exp. N°</th>
+                <th>Oficina</th>
+                <th>Usuario</th>
+              </tr>
+            </thead>
+        <tbody>
+          <tr>
+              <td>Enviar Consulta</td>
+              <td>42</td>
+              <td>06/09/2011</td>
+              <td>APROBADA</td>
+              <td>1-5049-2011</td>
+              <td>MANTENIMIENTO - CASA CENTRAL</td>
+              <td>Anonimo</td>
+          </tr>
+          <tr>
+            <td>Enviar Consulta</td>
+            <td>41</td>
+            <td>05/09/2011</td>
+            <td>APROBADA</td>
+            <td>1-2536-2011</td>
+            <td>GERENCIA DEL C.C.E. - CASA CENTRAL  </td>
+            <td>Anonimo</td>
+          </tr>
+        </tbody>
     </table>
+  </div>
   </div>
       
 
-      <div class="row">
+  <div class="row">
+    <div class="span12">
      <div class="pagination">
         <ul>
           <li><a href="#">Prev</a></li>
@@ -82,4 +86,5 @@
           <li><a href="#">Next</a></li>
         </ul>
       </div>
-      </div>
+    </div>
+  </div>
