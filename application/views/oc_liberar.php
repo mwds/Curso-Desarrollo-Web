ed8cc0648f740bde44ce5c51eaca28ec8b2f5357
Patricio Ontiveros <ontiverospatricio@gmail.com>
2012-10-01 16:39:33 -0300

    Correcciones en maquetacion de OC_LIBERAR y PEDIDOS_VIEW

--- application/views/oc_liberar.php
+++ application/views/oc_liberar.php
@@ -30,51 +30,50 @@
               <td>Estado</td>
               <td></td>
             </tr>
-          </thead>
             <tbody>
               <tr>
-                <td><i class="icon-list"></i></td>
-                <td><i class="icon-pencil"></i></td>
+                <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
+                <td><button class="btn" type="button"><i class="icon-pencil"></i></button></td>
                 <td>06/09/2011</td>
                 <td>COPITEC LIBRERIA -DE STAMP S.R.L.-</td>
                 <td>EN CONFIGURACION</td>
                 <td><button class="btn btn-primary" type="submit">Liberar</button></td>
               </tr>
               <tr>
-                <td><i class="icon-list"></i></td>
-                <td><i class="icon-pencil"></i></td>
+                <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
+                <td><button class="btn" type="button"><i class="icon-pencil"></i></button></td>
                 <td>06/09/2011</td>
                 <td>LIBRERIA JUNIN -DE PABLO ANTONIO LEON-</td>
                 <td>EN CONFIGURACION</td>
                 <td><button class="btn btn-primary" type="submit">Liberar</button></td>
               </tr>
               <tr>
-                <td><i class="icon-list"></i></td>
-                <td><i class="icon-pencil"></i></td>
+                <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
+                <td><button class="btn" type="button"><i class="icon-pencil"></i></button></td>
                 <td>06/09/2011</td>
                 <td>AHUALLI MARI</td>
                 <td>EN CONFIGURACION</td>
                 <td><button class="btn btn-primary" type="submit">Liberar</button></td>
               </tr>
               <tr>
-                <td><i class="icon-list"></i></td>
-                <td><i class="icon-pencil"></i></td>
+                <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
+                <td><button class="btn" type="button"><i class="icon-pencil"></i></button></td>
                 <td>06/09/2011</td>
                 <td>ALUBA</td>
                 <td>EN CONFIGURACION</td>
                 <td><button class="btn btn-primary" type="submit">Liberar</button></td>
               </tr>
               <tr>
-                <td><i class="icon-list"></i></td>
-                <td><i class="icon-pencil"></i></td>
+                <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
+                <td><button class="btn" type="button"><i class="icon-pencil"></i></button></td>
                 <td>06/09/2011</td>
                 <td>ABC S.A.</td>
                 <td>EN CONFIGURACION</td>
                 <td><button class="btn btn-primary" type="submit">Liberar</button></td>
               </tr>
               <tr>
-                <td><i class="icon-list"></i></td>
-                <td><i class="icon-pencil"></i></td>
+                <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
+                <td><button class="btn" type="button"><i class="icon-pencil"></i></button></td>
                 <td>06/09/2011</td>
                 <td>PINTURERIA ESPAÑA S.R.L.</td>
                 <td>EN CONFIGURACION</td>
--- application/views/pedidos_view.php
+++ application/views/pedidos_view.php
@@ -1,1 +1,118 @@
-pedidos - ViEW
+<div class="row">
+	<div class="span12"><h4>Pedidos No Planificados</h4></div>
+</div>
+<div class="row">
+	<div class="span12">
+		<form class="form-horizontal">
+			<div class="control-group">
+				<label class="control-label" for="inputNroPedido">Nro. Pedido:</label>
+				<div class="controls">
+					<input type="text" id="inputNroPedido" placeholder="Nro Pedido">
+				</div>		
+			</div>
+		</form>
+	</div>
+</div>
+<div class="row">
+	<div class="span12">
+		<form class="form-horizontal">
+			<div class="control-group">
+				<label class="control-label" for="inputEstado">Estado:</label>
+				<div class="controls">
+					<select>
+						<option>Anulada</option>
+						<option>Aprobada</option>
+						<option>Con Orden de Compra</option>
+						<option>Generada</option>
+						<option>Iniciada</option>
+						<option>Recepcionada</option>
+						<option>Revisada</option>
+						<option>Todos</option>						
+					</select>					
+				</div>		
+			</div>
+		</form>
+	</div>
+</div>
+<div class="row">
+	<div class="span12">
+		<div class="pagination">
+          	<ul>
+	            <li><a href="#">Prev</a></li>
+	            <li><a href="#">1</a></li>
+	            <li><a href="#">2</a></li>
+	            <li><a href="#">3</a></li>
+	            <li><a href="#">4</a></li>
+	            <li><a href="#">Next</a></li>
+	        </ul>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="span12">
+        <table class="table table-bordered">
+            <tr class="info">
+	            <td></td>
+    	        <td></td>
+        	    <td>O.C.</td>
+            	<td>Cod. Ped.</td>
+            	<td>Fec. Carga</td>
+            	<td>Fec. Vencimiento</td>
+            	<td>Estado</td>
+            	<td>Observación</td>
+          	</tr>
+        <tbody>
+        <tr>
+            <td><button class="btn" type="button"><i class="icon-edit"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-print"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+            <td>45</td>
+            <td>08/09/2011</td>
+            <td>15/09/2011</td>
+            <td>Con Orden de Compra</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><button class="btn" type="button"><i class="icon-edit"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-print"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+            <td>44</td>
+            <td>07/09/2011</td>
+            <td>14/09/2011</td>
+            <td>Con Orden de Compra</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><button class="btn" type="button"><i class="icon-edit"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-print"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+            <td>43</td>
+            <td>07/09/2011</td>
+            <td>14/09/2011</td>
+            <td>Aprobada</td>
+            <td>Pedido de Prueba 2011</td>
+        </tr>
+        <tr>
+            <td><button class="btn" type="button"><i class="icon-edit"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-print"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+            <td>42</td>
+            <td>06/09/2011</td>
+            <td>13/09/2011</td>
+            <td>Aprobada</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><button class="btn" type="button"><i class="icon-edit"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-print"></i></button></td>
+            <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+            <td>41</td>
+            <td>05/09/2011</td>
+            <td>12/09/2011</td>
+            <td>Con Orden de Compra</td>
+            <td></td>
+        </tr>
+    	</table>
+    </div>
+</div>
+                               
