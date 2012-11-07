0d825d7d7152aa353407f29a82e1de9cb4f2f7a4
Patricio Ontiveros <ontiverospatricio@gmail.com>
2012-11-06 23:06:31 -0300

    Modificacion de tablas en oc_abm y pedidos_view

--- application/views/oc_abm.php
+++ application/views/oc_abm.php
@@ -24,7 +24,6 @@
                 <td></td>
                 <td>Nro. N.P.</td>
                 <td>Fec. Carga</td>
-                <td>Estado</td>
                 <td>Expediente Nro.</td>
                 <td>Oficina</td>
                 <td>Usuario</td>
@@ -35,28 +34,222 @@
                     <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
                     <td>41</td>
                     <td>05/09/2011</td>
-                    <td>Aprobada</td>
                     <td>1-2536-2011</td>
                     <td>Gerencia del C.C.E. - Casa Central</td>
-                    <td>Salazar Guemes Marianella</td>
+                    <td>Anonimo</td>
                 </tr>
                 <tr>
                     <td>
-
+                        <table class="table table-striped">
+                            <thead>
+                                <tr class="info">
+                                    <td>Producto</td>
+                                    <td>Presentación</td>
+                                    <td>Cantidad</td>
+                                    <td>Proveedor</td>
+                                    <td>Precio Unitario</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="pagination">
+                            <ul>
+                                <li><a href="#">Prev</a></li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                <li><a href="#">Next</a></li>
+                            </ul>
+                        </div>
+                        <button class="btn" type="submit">Liberar O.C.</button>
                     </td>
                 </tr>
                 <tr>
                     <td><button class="btn" type="button"><i class="icon-list"></i></button></td>
                     <td>42</td>
                     <td>06/09/2011</td>
-                    <td>Aprobada</td>
                     <td>1-5049-2011</td>
                     <td>Mantenimiento - Casa Central</td>
-                    <td>Ayup Cynthia Ines</td>
+                    <td>Anonimo</td>
                 </tr>
                 <tr>
                     <td>
-                        
+                        <table class="table table-striped">
+                            <thead>
+                                <tr class="info">
+                                    <td>Producto</td>
+                                    <td>Presentación</td>
+                                    <td>Cantidad</td>
+                                    <td>Proveedor</td>
+                                    <td>Precio Unitario</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>                                        
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </td>
+                                    <td><input type="text" class="input-preciounitario" placeholder="Precio Unitario"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="pagination">
+                            <ul>
+                                <li><a href="#">Prev</a></li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                <li><a href="#">Next</a></li>
+                            </ul>
+                        </div>
+                        <button class="btn" type="submit">Liberar O.C.</button>
                     </td>
                 </tr>                
             </tbody>
@@ -76,5 +269,5 @@
 	        </ul>
         </div>
     </div>
-    <div class="span2"><button class="btn btn-primary" type="button">Previa OC</button></div>
+    
 </div>
--- application/views/pedidos_view.php
+++ application/views/pedidos_view.php
@@ -2,12 +2,22 @@
 	<div class="span12"><h4>Pedidos No Planificados</h4></div>
 </div>
 <div class="row">
-	<div class="span12">
+	<div class="span4">
 		<form class="form-horizontal">
 			<div class="control-group">
 				<label class="control-label" for="inputNroPedido">Nro. Pedido:</label>
 				<div class="controls">
 					<input type="text" id="inputNroPedido" placeholder="Nro Pedido">
+				</div>		
+			</div>
+		</form>
+	</div>
+	<div class="span8">
+		<form class="form-horizontal">
+			<div class="control-group">
+				<label class="control-label" for="inputNroExpediente">Nro. Expediente:</label>
+				<div class="controls">
+					<input type="text" id="inputNroExpediente" placeholder="Nro Expediente">
 				</div>		
 			</div>
 		</form>
@@ -54,32 +64,124 @@
             <thead>
                 <tr class="info">
                     <td></td>
-                    <td></td>
-                    <td>O.C.</td>
-                    <td>Cod. Ped.</td>
+                    <td>Nro. Ped.</td>
                     <td>Fec. Carga</td>
                     <td>Fec. Vencimiento</td>
                     <td>Estado</td>
-                    <td>Observación</td>
                 </tr>
             </thead>
             <tbody>
 
         		<?php #foreach ($pedidos as $pedidos_item): ?>
 
-                  <tr>
-                      <td><button class="btn" type="button"><i class="icon-edit"></i></button></td>
-                      <td><button class="btn" type="button"><i class="icon-print"></i></button></td>
-                      <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
-                      <td><?php #echo $pedidos_item['pedido_id'] ?></td>
-                      <td><?php #echo $pedidos_item['pedido_name'] ?></td>
-                      <td><?php #echo $pedidos_item['pedido_estado'] ?></td>
-                      <td><a href="pedidos/<?php #echo $pedidos_item['pedido_id'] ?>">Ver Pedido</a></td>
-                      <td></td>
-                  </tr>
+	            <tr>
+	                <td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+	                <td><?php #echo $pedidos_item['pedido_id'] ?></td>
+	                <td><?php #echo $pedidos_item['pedido_name'] ?></td>
+	                <td><?php #echo $pedidos_item['pedido_estado'] ?></td>
+	                <td><a href="pedidos/<?php #echo $pedidos_item['pedido_id'] ?>">Ver Pedido</a></td>
+	            </tr>
                 
         		<?php #endforeach ?>
-            
+            	
+            	<tr>
+            		<td>
+            			<div class="span12">
+	            			<label class="control-label" for="inputobservaciones">Observaciones: </label>
+	            			<textarea rows="3"></textarea>
+	            		</div>
+	            		<br>
+	            		<div class="span2">
+	            			<label class="radio"><input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Productos</label>
+						</div>
+						<div class="span6">
+							<label class="radio"><input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Orden de Compra</label>
+						</div>
+						<br>
+					    <table class="table table-bordered table-striped">
+				            <thead>
+				                <tr class="info">
+				                    <td>Nombre</td>
+				                    <td>Presentación</td>
+				                    <td>Cantidad</td>
+				                </tr>
+				            </thead>
+				            <tbody>
+				            	<tr>
+				                	<td></td>
+				                	<td></td>
+				                	<td></td>
+				            	</tr>
+		            		</tbody>
+		            	</table>
+					    <table class="table table-bordered table-striped">
+				            <thead>
+				                <tr class="info">
+				                    <td></td>
+				                    <td>Nro O.C.</td>
+				                    <td>Proveedor</td>
+				                    <td>Total</td>
+				                    <td>Usuario</td>
+				                </tr>
+				            </thead>
+				            <tbody>
+				            	<tr>
+				                	<td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+				                	<td>0010</td>
+				                	<td>Chanchito S.A.</td>
+				                	<td>13,69</td>
+				                	<td>Auil Jorge Gilberto</td>
+				            	</tr>
+				            	<tr>
+				            		<td>
+									    <table class="table table-bordered table-striped">
+								            <thead>
+								                <tr class="info">
+								                    <td>Nombre</td>
+								                    <td>Presentación</td>
+								                    <td>Cantidad</td>
+								                </tr>
+								            </thead>
+								            <tbody>
+								            	<tr>
+								                	<td></td>
+								                	<td></td>
+								                	<td></td>
+								            	</tr>
+						            		</tbody>
+						            	</table>
+				            		</td>
+				            	</tr>
+				            	<tr>
+				                	<td><button class="btn" type="button"><i class="icon-arrow-down"></i></button></td>
+				                	<td>0011</td>
+				                	<td>Tutetamimoso S.A.</td>
+				                	<td>0,59</td>
+				                	<td>Auil Jorge Gilberto</td>
+				            	</tr>
+				            	<tr>
+				            		<td>
+									    <table class="table table-bordered table-striped">
+								            <thead>
+								                <tr class="info">
+								                    <td>Nombre</td>
+								                    <td>Presentación</td>
+								                    <td>Cantidad</td>
+								                </tr>
+								            </thead>
+								            <tbody>
+								            	<tr>
+								                	<td></td>
+								                	<td></td>
+								                	<td></td>
+								            	</tr>
+						            		</tbody>
+						            	</table>
+				            		</td>
+				            	</tr>
+		            		</tbody>
+		            	</table>
+            	</tr>
             </tbody>
 
     	</table>
